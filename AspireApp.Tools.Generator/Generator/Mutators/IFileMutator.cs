35c3c236f2f99f46c10ba47dfbbdb19c1612701f
Nagu <nahuegallinoti@gmail.com>
2026-06-02 06:45:54 +0200

    Quitada tool del generador de código

--- AspireApp.Tools.Generator/Generator/Mutators/IFileMutator.cs
+++
@@ -1,9 +0,0 @@
-namespace AspireApp.Tools.Generator.Generator.Mutators;
-
-internal interface IFileMutator
-{
-    string TargetPath { get; }
-    MutationResult Mutate(string source, EntitySpec entity);
-}
-
-internal sealed record MutationResult(string NewContent, bool Changed, string Description);
